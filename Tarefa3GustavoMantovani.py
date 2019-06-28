96193f822723c4e1d8957f21efcdf6ac3850d04c
Gustavo Mantovani Fonseca <manto.fon@gmail.com>
2019-06-28 20:03:30 -0300

    Add files via upload

---
+++ Tarefa3GustavoMantovani.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 28 19:33:43 2019
+
+@author: gustavo.fonseca
+"""
+
+'''Esse é um script criado para transformar horas em segundos. 
+As horas serão representadas pela variável h.
+Os segundos serão representados pela variável s.'''
+
+#Exercício 1:
+h1=1.5 #O número 1 representa o primeiro exercício da tarefa
+s=h1*3600 #Usaremos essa multiplicação pois cada hora equivale a 3600 segundos.
+print("1. Uma hora e meia contém",int(s),"segundos.")
+
+
+#Exercício 2:
+h2=12 #Assim como no caso anterior, o número 2 representa o segundo exercício.
+s=h2*3600
+print("2. 12 horas contém",int(s),"segundos.")
+
+
+#Exercício 3:
+h3=24
+s=h3*3600
+print("3. 24 horas contém",int(s),"segundos.")
